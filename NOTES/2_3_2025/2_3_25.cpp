47b8d58d1a89dd59a83b8c17ebc1ad37ff529bad
Vicky Heredia <87664447+vicky04edx@users.noreply.github.com>
2025-02-03 21:17:54 +0000

    edit

--- NOTES/2_3_2025/2_3_25.cpp
+++ NOTES/2_3_2025/2_3_25.cpp
@@ -1,11 +1,69 @@
 # include <iostream>
 using namespace std;
 
-bool in_array(int *a,int index)
+bool in_array(int *a,int x, int index)
 {
-    for ()
+    for (int i=0; i<=index; i++)
+    {
+        if(a[i]==x)
+        {
+            return true;
+        }
+      
+    }
+      return false;
 }
 
+bool print()
+{
+    for (int j=0; j<=size; j++)
+    {
+        cout << a[size] << " ";
+        if (i+1%15)
+    }
+}
+
+int binary_search(int *a, int key, int size)
+{
+    int f = 0;
+    int r = size - 1;
+    int m (f + r)/2;
+    bool found = false;
+    while(!found)
+    {
+        if(f==r==m)
+        {
+            return -1;
+        }
+        if (a[f] == key)
+        {
+            return f;
+        }
+         if (a[m] == key)
+        {
+            return r;
+        }
+         if (a[r] == key)
+        {
+            return r;
+        }
+         if (key >a[m])
+        {
+            f= m+1;
+
+        }
+        else
+        {
+            r = m + 1; 
+        }
+        m = (f+r)/2
+    }
+}
+void swap (int *a, int i, int j)
+{
+
+    int temp = a[i];
+}
 int main()
 {
     int size = 100;
@@ -16,22 +74,17 @@ int main()
     for (int i=0; i<size; i++)
     {
        
-        unique = true;
-        while(unique)
+        r = rand()%10000;
+        while(in_array(nums, r, i))
         {
-            int r = rand()%10000
 
-            for (int j=0; j<=i; j++)
-            {
-                if(nums[j] == r)
-                {
-                    unique = false;
-                    break;
-                }
-            }
+            r = rand()%10000;
         }
 
         nums[i] = r;
     }
-
+    print(nums, size);
+    cout << endl;
+    bubble_sort(nums,size);
+    print(nums, size);
 }
